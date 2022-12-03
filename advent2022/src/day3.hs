fcac2a8e8152c0e68ae249107816be380185e11f
Weiwei Chen <weiwei.chen@sambanovasystems.com>
2022-12-03 18:37:54 -0500

    Day 3, 2022.

--- advent2022/src/day3.hs
+++ advent2022/src/day3.hs
@@ -6,11 +6,30 @@ import qualified Data.Text.IO as Text
 
 main = do
   part1
+  part2
+
+common xs ys = [ x | x <- xs , y <- ys, x == y]
+common3 xs ys zs = [ x | x <- xs , y <- ys, z <- zs, x == y && x == z && y == z]
 
 part1 = do
-    input_list <- fmap Text.lines (Text.readFile "../../data/day2.txt")
-    let r0 = map (\line -> Split.splitOn " " (Text.unpack line)) input_list
-    let r1 = map(\pair -> ((fight1 (pair !! 0) (pair !! 1)), value1 (pair !! 1))) r0
-    let result = sum(map (\t -> fst t + snd t) r1)
-    print result
+    input_list <- fmap Text.lines (Text.readFile "../../data/day3.txt")
+    let r0 = map(\line -> splitAt ((Text.length line)  `div` 2) (Text.unpack line)) input_list
+    let r1 = map(\pair -> common (fst pair) (snd pair)) r0
+    let r2 = map(\c -> if fromEnum (c !! 0) < 91 then fromEnum(c!!0) - 38 else fromEnum(c!!0) - 96) r1
+    print (sum r2)
+
+mygroup :: Int -> [a] -> [[a]]
+mygroup _ [] = []
+mygroup n l
+  | n > 0 = (take n l) : (mygroup n (drop n l))
+  | otherwise = error "Negative or zero n"
+
+part2 = do
+    input_list <- fmap Text.lines (Text.readFile "../../data/day3.txt")
+    let r0 = mygroup 3 input_list
+    let r1 = map(\list -> map(\t -> Text.unpack t) list) r0
+    let c = common3 ((r1 !! 0)!!0) ((r1 !! 0)!!1) ((r1 !! 0)!!2)
+    let r2 = map(\list -> common3 (list !!0) (list !! 1) (list !! 2)) r1
+    let r3 = map(\c -> if fromEnum (c !! 0) < 91 then fromEnum(c!!0) - 38 else fromEnum(c!!0) - 96) r2
+    print (sum r3)
 
