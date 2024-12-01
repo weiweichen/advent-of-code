2bdcfec6622d48b346ec491520f80340b05d71f1
Weiwei Chen <weiwei.chen@modular.com>
2024-12-01 16:18:25 -0500

    Start 2024.

---
+++ advent2024/src/main/scala/Main.scala
@@ -0,0 +1,4 @@
+import day1.*
+
+@main
+def run(): Unit = day1.solution("data/day1/input.txt").run()
---
+++ advent2024/src/main/scala/day1/solution.scala
@@ -0,0 +1,27 @@
+package day1
+
+import scala.io.Source._
+
+class solution(input: String):
+  def prob1(): Unit =
+    val lines = fromFile(input).getLines
+    val s = lines.map(line => line.split(" +").map(w => w.toInt))
+    val (l, r) = s.flatMap(n => n).zipWithIndex.partition(_._2 % 2 == 0)
+    val sortedLeft = l.map((value, index) => value).toList.sorted
+    val sortedRight = r.map((value, index) => value).toList.sorted
+    println("Puzzle 1: ")
+    println(sortedLeft.zip(sortedRight).map((l, r) => (l - r).abs).sum)
+
+  def prob2(): Unit =
+    val lines = fromFile(input).getLines
+    val s = lines.map(line => line.split(" +").map(w => w.toInt))
+    val (l, r) = s.flatMap(n => n).zipWithIndex.partition(_._2 % 2 == 0)
+    val left = l.map((value, index) => value).toList
+    val histogramRight =
+      r.map((value, index) => value).toList.groupBy(l => l).map((key, value) => (key, value.size)).toMap
+    println("Puzzle 2: ")
+    println(left.map(l => l * histogramRight.getOrElse(l, 0)).sum)
+
+  def run(): Unit =
+    prob1()
+    prob2()
