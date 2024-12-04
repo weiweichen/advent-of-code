360d23f9ae17f021117e3b590d71e8a1756b3aee
Weiwei Chen <weiwei.chen@modular.com>
2024-12-04 00:53:51 -0500

    2024 Day4.

--- advent2024/src/main/scala/Main.scala
+++ advent2024/src/main/scala/Main.scala
@@ -1,9 +1,11 @@
 import day1.*
 import day2.*
 import day3.*
+import day4.*
 
 @main
 def run(): Unit =
   // day1.solution("data/day1/input.txt").run()
   // day2.solution("data/day2/input.txt").run()
-  day3.solution("data/day3/input.txt").run()
+  // day3.solution("data/day3/input.txt").run()
+  day4.solution("data/day4/input.txt").run()
---
+++ advent2024/src/main/scala/day4/solution.scala
@@ -0,0 +1,54 @@
+package day4
+import scala.io.Source._
+
+class solution(input: String):
+  def matchLines(lines: List[String]): Int =
+    lines.map(l => {
+      val pattern = "XMAS".r
+      val m1 = pattern.findAllIn(l)
+      val m2 = pattern.findAllIn(l.reverse)
+      m1.size + m2.size
+    }).sum
+
+  def matchXMASGrid(lines: List[String]): Int =
+    (0 to lines.head.size - 4).iterator.map(i => {
+      (0 to lines.size - 4).map(j =>  {
+        var r = 0
+        if (lines(j)(i) == 'X' && lines(j+1)(i+1) == 'M' && lines(j+2)(i+2) == 'A' && lines(j+3)(i+3) == 'S')
+          r = r + 1
+        if (lines(j)(i) == 'S' && lines(j + 1)(i + 1) == 'A' && lines(j + 2)(i + 2) == 'M' && lines(j + 3)(i + 3) == 'X')
+          r = r + 1
+        if (lines(j)(i+3) == 'X' && lines(j + 1)(i + 2) == 'M' && lines(j + 2)(i + 1) == 'A' && lines(j + 3)(i) == 'S')
+          r = r + 1
+        if (lines(j)(i+3) == 'S' && lines(j + 1)(i + 2) == 'A' && lines(j + 2)(i + 1) == 'M' && lines(j + 3)(i) == 'X')
+          r = r + 1
+        r
+      }).sum
+    }).sum
+
+  def matchMASGrid(lines: List[String]): Int =
+    (0 to lines.head.size - 3).iterator.map(i => {
+      (0 to lines.size - 3).map(j => {
+        val b1 = (lines(j)(i) == 'M' && lines(j + 1)(i + 1) == 'A' && lines(j + 2)(i + 2) == 'S')
+           || (lines(j)(i) == 'S' && lines(j + 1)(i + 1) == 'A' && lines(j + 2)(i + 2) == 'M')
+        val b2 = (lines(j)(i+2) == 'M' && lines(j + 1)(i + 1) == 'A' && lines(j + 2)(i) == 'S')
+          || (lines(j)(i+2) == 'S' && lines(j+ 1)(i + 1) == 'A' && lines(j + 2)(i) == 'M')
+        if(b1 && b2) 1 else 0
+      }).sum
+    }).sum
+
+  def prob1(): Int =
+    val lines = fromFile(input).getLines.toList
+    matchLines(lines) + matchLines(lines.transpose.map(l => l.mkString)) + matchXMASGrid(lines)
+
+  def prob2(): Int =
+    val lines = fromFile(input).getLines.toList
+    matchMASGrid(lines)
+
+  def run(): Unit =
+    print("Puzzle 1: ")
+    println(prob1())
+
+    print("Puzzle 2: ")
+    println(prob2())
+
