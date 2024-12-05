7f6fd43717c7415c13d8fec98fe241747f4a4364
Weiwei Chen <weiwei.chen@modular.com>
2024-12-05 01:21:37 -0500

    2024 Day5.

--- advent2024/src/main/scala/Main.scala
+++ advent2024/src/main/scala/Main.scala
@@ -2,10 +2,12 @@ import day1.*
 import day2.*
 import day3.*
 import day4.*
+import day5.*
 
 @main
 def run(): Unit =
   // day1.solution("data/day1/input.txt").run()
   // day2.solution("data/day2/input.txt").run()
   // day3.solution("data/day3/input.txt").run()
-  day4.solution("data/day4/input.txt").run()
+  // day4.solution("data/day4/input.txt").run()
+  day5.solution("data/day5/input.txt").run()
---
+++ advent2024/src/main/scala/day5/solution.scala
@@ -0,0 +1,59 @@
+package day5
+
+import scala.io.Source.*
+import collection.mutable.HashSet
+import scala.collection.mutable
+
+class solution(input: String):
+  
+  def getOrdered(lines: List[String]): List[mutable.HashSet[Int]] =
+    val orders = List.fill(100) {mutable.HashSet[Int]()}
+    var parseOrder = true
+    lines.indices.iterator.foreach(i => {
+      if (lines(i).isEmpty) parseOrder = false
+      if (parseOrder) {
+        val s = lines(i).split('|').map(s => s.toInt)
+        orders(s(0)).update(s(1), true)
+      }
+    })
+    orders
+
+  def prob1(): Int =
+    val lines = fromFile(input).getLines.toList
+    val orders = getOrdered(lines)
+    lines.map(l => {
+      if(l.isEmpty || l.split('|').length > 1 ) 0
+      else {
+        val seq = l.split(",").map(s => s.toInt)
+        val sorted = seq.sortWith((i, j) => {
+          orders(i).contains(j)
+        })
+        if(sorted.sameElements(seq)) sorted(sorted.length/2)
+        else 0
+      }
+    }).sum
+
+  def prob2(): Int =
+    val lines = fromFile(input).getLines.toList
+    val orders = getOrdered(lines)
+    lines.map(l => {
+      if (l.isEmpty || l.split('|').length > 1) 0
+      else {
+        val seq = l.split(",").map(s => s.toInt)
+        val sorted = seq.sortWith((i, j) => {
+          orders(i).contains(j)
+        })
+        if (!sorted.sameElements(seq)) sorted(sorted.length/ 2)
+        else 0
+      }
+    }).sum
+
+  def run(): Unit =
+    println("===== Day 5 =====")
+    print("Puzzle 1: ")
+    println(prob1())
+
+    print("Puzzle 2: ")
+    println(prob2())
+
+
